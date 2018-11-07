cb96267864c4be4721acc9dcefb9baa38d4c84a3
Yenck <kshs010239@gmail.com>
2018-11-07 15:42:32 +0800

    config version

--- automl_vision.py
+++ automl_vision.py
@@ -3,13 +3,6 @@
 # TODO(developer): Uncomment and set the following variables
 
 
-project_id = 'PROJECT_ID_HERE'
-compute_region = 'COMPUTE_REGION_HERE'
-dataset_name = 'DATASET_NAME_HERE'
-dataset_id = 0
-data_path = ""
-multilabel = 0#True for multilabel or False for multiclass
-model_id = 0
 
 from google.cloud import automl_v1beta1 as automl
 from google.cloud.automl_v1beta1 import enums
@@ -23,38 +16,83 @@ client = automl.AutoMlClient()
 automl_client = client
 
 # A resource that represents Google Cloud Platform location.
-
-def require():
-    #project_id = input('PROJECT_ID: ')
-    #compute_region = input('COMPUTE_REGION: ')
-    #dataset_name = input('DATASET_NAME: ')
-    #multilabel = input('MULTILABLE?(y/n): ')[0] == 'y' ? True : False
-    if len(argv) < 6:
-        print("invalid argu")
+def read_config():
+    config = dict()
+    try:
+        with open(argv[2], "r") as f:
+            for line in f:
+                if ':' not in line:
+                    continue
+                name, data = line.split(':')[:2]
+                data = data.split()
+                if len(data) > 0:
+                    config[name] = data[0] 
+    except IOError:
+        print('File "', argv[2], '" is not found.', sep = "")
         exit()
-    global project_id, compute_region, dataset_name, data_path
-    project_id = argv[2]
-    compute_region = argv[3]
-    dataset_name = argv[4]
-    data_path = argv[5]
 
+    return config 
+
+def get(Dict, key, default = None):
+    if key not in Dict or Dict[key] == "":
+        return default
+    if Dict[key] == "True":
+        return True
+    return Dict[key]
 
 def main():
-    global project_id, compute_region, model_id, data_path
-    if len(argv) < 2:
+    if len(argv) < 3:
         print("automl_vision.py: invalid argv")
         exit()
 
-    if argv[1] == 'init':
-        require()
-        print("start")
+    if argv[1] == 'config':
+        with open(argv[2], "w") as f:
+            print("*** Don't input whitespace. ***")
+            print("[Required]")
+            required = ["project_id: ", "dataset_name: "]
+            for item in required:
+                while 1:
+                    answer = input(item)
+                    if len(answer.split()) > 0:
+                        f.write(item + answer + "\n")
+                        break
+            required2 = ["multilabel"]
+            for item in required2:
+                answer = input(item + "?(y/n):")
+                if len(answer.split()) > 0 and (answer[0] == "y" or answer[0] == "Y"):
+                    f.write(item + ": True\n")
+
+
+            print()
+            print("[Optional]")
+            print("[enter] will use default setting.")
+            optional = ["model_name: "]
+            for item in optional:
+                answer = input(item)
+                f.write(item + answer + "\n")
+
+        exit()
+
+    config = read_config()
+    project_id = get(config, "project_id")
+    compute_region = get(config, "compute_region", "us-central1")
+    dataset_name = get(config, "dataset_name")
+    model_name = get(config, "model_name", dataset_name)
+    model_id = get(config, "model_id")
+    multilabel = get(config, "multilabel", False)
+    
+    if argv[1] == 'create':
+        data_path = argv[3]
+
+        dataset_id = create_dataset(project_id, compute_region, dataset_name, multilabel)
+
         create_csv(project_id, dataset_name, data_path)
         remote_data_path = upload_data(project_id, dataset_name, data_path, "/tmp/" + dataset_name + '.csv')
         
-        create_dataset()
-        import_data(remote_data_path)
-        operation_id = train_model()
-        print("operation_id:", operation_id)
+        import_data(project_id, compute_region, dataset_id, remote_data_path)
+        model_id = train_model(project_id, compute_region, dataset_id, dataset_name)
+        with open(argv[2], "a") as f:
+            f.write("model_id: " + model_id + "\n")
 
         evaluate_model()
    
@@ -64,26 +102,23 @@ def main():
 
 
     elif argv[1] == 'predict':
-        project_id = argv[2]
-        compute_region = argv[3]
-        model_id = argv[4]
-        data_path = argv[5]
+        data_path = argv[3]
         if os.path.isfile(data_path):
-            predict(argv[5])
+            predict(project_id, compute_region, model_id, data_path)
             
         else:
-            for root, dir, files in os.walk(argv[5]):
+            for root, dir, files in os.walk(data_path):
                 for f in files:
-                    filename = os.path.join(root, f)
-                    if len(filename) == 0 or not os.path.isfile(filename):
+                    file_path = os.path.join(root, f)
+                    if len(file_path) == 0 or not os.path.isfile(file_path):
                         continue
-                    predict(filename)
+                    predict(project_id, compute_region, model_id, file_path)
     else:
         print('Command "' + argv[1] + '" not found')
 
 
 
-def create_dataset():
+def create_dataset(project_id, compute_region, dataset_name, multilabel = False):
     project_location = client.location_path(project_id, compute_region)
 
     # Classification type is assigned based on multilabel value.
@@ -117,7 +152,7 @@ def create_dataset():
     return dataset_id   
 
 
-def import_data(path):
+def import_data(project_id, compute_region, dataset_id, path):
     # Get the full path of the dataset.
     dataset_full_id = client.dataset_path(
         project_id, compute_region, dataset_id
@@ -127,19 +162,21 @@ def import_data(path):
     input_uris = path.split(",")
     input_config = {"gcs_source": {"input_uris": input_uris}}
 
-    # Import data from the input URI.
+    print(dataset_full_id)
+    print(input_config)
+
+
     response = client.import_data(dataset_full_id, input_config)
 
     print("Processing import...")
-    # synchronous check of operation status.
     print("Data imported. {}".format(response.result()))
 
 
-def train_model():
+def train_model(
+    project_id, compute_region, dataset_id, model_name, train_budget = 1, wait_for_train = True
+):
     project_location = client.location_path(project_id, compute_region)
-    model_name = dataset_name
     # Set model name and model metadata for the image dataset.
-    train_budget = 1
     my_model = {
         "display_name": model_name,
         "dataset_id": dataset_id,
@@ -148,14 +185,19 @@ def train_model():
         else {},
     }
 
-    # Create a model with the model metadata in the region.
     response = client.create_model(project_location, my_model)
     op = response.operation
 
 
     print("Training operation name: {}".format(response.operation.name))
-    print("Training started...")
-    return response.operation.name
+    print("Training started")
+    print("operation id: ", response.operation.name)
+    if wait_for_train:
+        model_id = str(response.result()).split('/')[-1][:-2]
+        print("model id:", model_id)
+        return model_id
+
+
 
 def check_op(operation_id):
     output = os.popen('curl \
@@ -181,84 +223,46 @@ def check_op(operation_id):
         print('Not done yet.')
 
 
-
-def check_model(model_id):
-	# Get the full path of the model.
-	model_full_id = client.model_path(project_id, compute_region, model_id)
-
-	# Get complete detail of the model.
-	model = client.get_model(model_full_id)
-
-	# Retrieve deployment state.
-	if model.deployment_state == enums.Model.DeploymentState.DEPLOYED:
-		deployment_state = "deployed"
-	else:
-		deployment_state = "undeployed"
-
-	# Display the model information.
-	print("Model name: {}".format(model.name))
-	print("Model id: {}".format(model.name.split("/")[-1]))
-	print("Model display name: {}".format(model.display_name))
-	print("Image classification model metadata:")
-	print(
-		"Training budget: {}".format(
-			model.image_classification_model_metadata.train_budget
-		)
-	)
-	print(
-		"Training cost: {}".format(
-			model.image_classification_model_metadata.train_cost
-		)
-	)
-	print(
-		"Stop reason: {}".format(
-			model.image_classification_model_metadata.stop_reason
-		)
-	)
-	print(
-		"Base model id: {}".format(
-			model.image_classification_model_metadata.base_model_id
-		)
-	)
-	print("Model create time:")
-	print("\tseconds: {}".format(model.create_time.seconds))
-	print("\tnanos: {}".format(model.create_time.nanos))
-	print("Model deployment state: {}".format(deployment_state))
-
 def evaluate_model():
     # TODO
     pass
 
-def predict(file_path):
+def predict(
+    project_id, compute_region, model_id, file_path, score_threshold="0.1"
+):
     model_full_id = automl_client.model_path(
         project_id, compute_region, model_id
     )
 
-    # Create client for prediction service.
     prediction_client = automl.PredictionServiceClient()
 
-    # Read the image and assign to payload.
     with open(file_path, "rb") as image_file:
         content = image_file.read()
     payload = {"image": {"image_bytes": content}}
 
-    # params is additional domain-specific parameters.
-    # score_threshold is used to filter the result
-    # Initialize params
     params = {}
-    score_threshold = "0.1"
     if score_threshold:
         params = {"score_threshold": score_threshold}
     
     response = prediction_client.predict(model_full_id, payload, params)
-    '''print("Prediction results:")
+    
+    '''
+    # full result
+    print("Prediction results:")
     for result in response.payload:
         print("Predicted class name: {}".format(result.display_name))
         print("Predicted class score: {}".format(result.classification.score))
     '''
-    print(file_path, end = '')
+
+    # csv result
+    output = []
     for result in response.payload:
-        print(",", result.display_name, end = '')
+        output.append((result.classification.score, result.display_name))
+
+    output.sort(reverse = True)
+    print(file_path, end = '')
+    for result in output:
+        print(",", result[1], end = '')
     print()
 
 if __name__ == "__main__" :
